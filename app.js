87f8d9dc4367be8066138cdca7fc4e3d8484601a
ThilakAswin <53222461+ThilakAswin@users.noreply.github.com>
2023-12-29 22:11:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/todolist");
+mongoose.connect("mongodb://localhost:27017/todolist");
 
 const itemsSchema = mongoose.Schema({
     name:String
